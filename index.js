39147f78b27fa4a1e68fc090a2b08235ab9134ce
Harlan Ray <harlanray@icloud.com>
2023-07-17 12:53:44 -0700

    update 7-17
    site structure reconfiguring.

--- index.js
+++ index.js
@@ -1,9 +1,45 @@
-// window.onSpotifyIframeApiReady = (IFrameAPI) => {
-//     const element = document.getElementById('embed-iframe');
-//     const options = {
-//         uri: 'spotify:episode:7makk4oTQel546B0PZlDM5'
-//       };
-//     const callback = (EmbedController) => {};
-//     IFrameAPI.createController(element, options, callback);
-//   };
+// import {computePosition, size} from '@floating-ui/dom';
+ 
+// computePosition(referenceEl, floatingEl, {
+//   middleware: [
+//     size({
+//       apply({availableWidth, availableHeight, elements,}) {
+//         // Do things with the data, e.g.
+//         Object.assign(elements.floating.style, {
+//           maxWidth: `${availableWidth}px`,
+//           maxHeight: `${availableHeight}px`,
+//         });
+//       },
+//     }),
+//   ],
+// });
+
+
+document.addEventListener('click', function (event) {
+    var target = event.target;
+    var navbar = document.querySelector('.navbar');
+    var isClickInsideNavbar = navbar.contains(target);
+    if (!isClickInsideNavbar) {
+      var collapses = navbar.querySelectorAll('.collapse');
+      collapses.forEach(function (collapse) {
+        var isExpanded = collapse.classList.contains('show');
+        if (isExpanded) {
+          collapse.classList.remove('show');
+        }
+      });
+    }
+  });
   
+  
+
+const links = document.querySelectorAll('.navbar .nav-link')
+links.forEach(function(link) {
+    link.addEventListener('click', function() {
+        let collapses = document.querySelectorAll('.collapse')
+        collapse.forEach(function(collapse) {
+            if(collapse.classList.contains('show')) {
+                collapse.classList.remove('show')
+            }
+        })
+    })
+})
